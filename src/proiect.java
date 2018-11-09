dd8da7037bc2f68efd1682a9e84dc96a48b6682d
Nicola <nicola.elena14@yahoo.com>
2018-11-09 18:01:51 +0200

    Import

---
+++ src/proiect.java
@@ -0,0 +1,16 @@
+public class proiect {
+
+
+    public static void main(String[] args) {
+        int myAge=32;
+        float weight=60.5f;
+                double height=356.6;
+                        long fidtance=673647533333666;
+                        char gender = 'm ';
+                        char test = 9 ;
+                        boolean student = false;
+                        boolean hasJob = true;
+                        int test2 = (int) weight
+                                System out 
+    }
+    }
