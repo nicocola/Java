f247cbc2e0d008ade4f465bf9472371a8ee506d7
Nicola <nicola.elena14@yahoo.com>
2018-11-09 19:28:15 +0200

    Import

--- src/proiect.java
+++ src/proiect.java
@@ -2,15 +2,29 @@ public class proiect {
 
 
     public static void main(String[] args) {
-        int myAge=32;
-        float weight=60.5f;
-                double height=356.6;
-                        long fidtance=673647533333666;
-                        char gender = 'm ';
-                        char test = 9 ;
-                        boolean student = false;
-                        boolean hasJob = true;
-                        int test2 = (int) weight
-                                System out 
+        int myAge = 32;
+        int anAge = myAge;
+        float weight = 60.5f;
+        double height = 356.6;
+        long fidtance = 673633666;
+        char gender = 'm';
+        char test = 9;
+        boolean student = false;
+
+        boolean hasJob = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.println("Hello");
+        String mesaj = "Hello";
+        String mesaj2 = "Goodbye";
+
+        System.out.println(mesaj);
+    double a =5;
+    int  b=2;
+    double c= a/b;
+    String mesaj3 =mesaj+mesaj2;
+
+        System.out.println(c);
+        System.out.println(mesaj3);
     }
-    }
+}
